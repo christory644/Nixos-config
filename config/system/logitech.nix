45a826a66d52ab9d0c52bdfb1af401be93a56c4c
Christopher Story <christory@pm.me>
2024-03-06 01:32:21 -0600

    make logitech support optional and flaggable via options.nix

--- config/system/logitech.nix
+++ config/system/logitech.nix
@@ -1,4 +1,7 @@
-{
+{ config, lib, ... }:
+
+let inherit (import ../../options.nix) enableLogitechSupport;
+in lib.mkIf (enableLogitechSupport == true) {
   hardware.logitech.wireless = {
     enable = true;
     enableGraphical = true;
