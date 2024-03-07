e7900c2e165982d147df42f811695679162ffd34
Christopher Story <christory@pm.me>
2024-03-06 19:01:18 -0600

    change some zsh settings

--- config/home/zsh.nix
+++ config/home/zsh.nix
@@ -3,6 +3,7 @@
 let inherit (import ../../options.nix)
   flakeDir
   hostname
+  userHome
   userShell;
 in lib.mkIf (userShell == "zsh") {
   programs.zsh = {
@@ -10,9 +11,8 @@ in lib.mkIf (userShell == "zsh") {
     syntaxHighlighting.enable = true;
     enableAutosuggestions = true;
     historySubstringSearch.enable = true;
+
     initExtra = ''
-      zstyle ":completion:*" menu select
-      zstyle ":completion:*" matcher-list "" "m:{a-z0A-Z}" "r:|=*" "l:|=* r:|=*"
       bindkey '^[[3~' delete-char                     # Key Del
       bindkey '^[[5~' beginning-of-buffer-or-history  # Key Page Up
       bindkey '^[[6~' end-of-buffer-or-history        # Key Page Down
@@ -25,14 +25,35 @@ in lib.mkIf (userShell == "zsh") {
         source $HOME/.zshrc-personal
       fi
     '';
-    initExtraFirst = ''
-      HISTFILE=~/.histfile
-      HISTSIZE=1000
-      SAVEHIST=1000
-      setopt autocd nomatch
-      unsetopt beep extendedglob notify
-      autoload -Uz compinit
-      compinit
+
+    history = {
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      ignoreSpace = true;
+      extended = true;
+      path = "${userHome}/.config/zsh/history";
+      share = false;
+      size = 100000;
+      save = 100000;
+    };
+
+    profileExtra = ''
+      setopt correct							# auto correct mistakes
+      setopt extendedglob						# extended globbing, allows regex with *
+      setopt nocaseglob							# case insensitive globbing
+      setopt numericglobsort						# sort filenames numerically when it makes sense
+      setopt appendhistory						# immediately append history instead of overwriting
+      unsetopt histignorealldups					# if a new command is a duplicate, do not remove the older one from history
+      zstyle ':completion:*' matcher-list "" 'm:{a-zA-Z}={A-Za-z}'       	# Case insensitive tab completion
+      zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"       	# Colored completion (different colors for dirs/files/etc)
+      zstyle ':completion:*' rehash true                              	# automatically find new executables in path
+      # Speed up completions
+      zstyle ':completion:*' accept-exact '*(N)'
+      zstyle ':completion:*' use-cache on
+      mkdir -p "$(dirname ${userHome}/.config/zsh/history)"
+      mkdir -p "$(dirname ${userHome}/.config/zsh/completion-cache)"
+      zstyle ':completion:*' cache-path "${userHome}/.config/zsh/completion-cache"
+      zstyle ':completion:*' menu select
     '';
     shellAliases = {
       sv = "sudo nvim";
