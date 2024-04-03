98f959e20c71e21799db1616bf6876e790fa00a7
Christopher Story <christory@pm.me>
2024-04-03 17:06:41 -0500

    add a de to the default non specialized boot entry

--- config/specializations/default.nix
+++ config/specializations/default.nix
@@ -1,4 +1,13 @@
+{ lib, config, pkgs, ... }:
 {
+  # declare a DE for non specialized boots
+  config = lib.mkIf (config.specialisation != {}) {
+    services.xserver.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+  };
+
+  # import configured specializations
   imports = [
     ./awesomewm.nix
     ./hyprland.nix
