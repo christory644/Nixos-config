ba7ecb8c491981c5c33974fa4ef77bbd68970d16
Christopher Story <christory@pm.me>
2024-04-05 01:47:32 -0500

    fix typo in nvidia-offload script definition

--- config/system/gpu/intelNvidia.nix
+++ config/system/gpu/intelNvidia.nix
@@ -5,7 +5,7 @@ in {
   environment.systemPackages =
     let nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
       export __NV_PRIME_RENDER_OFFLOAD=1
-      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA_G0
+      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
       export __GLX_VENDOR_LIBRARY_NAME=nvidia
       export __VK_LAYER_NV_optimus=NVIDIA_only
 
