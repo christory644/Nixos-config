6da901fcd1b050b3ef3171967cb0f04a9f202192
Christopher Story <christory@pm.me>
2024-06-17 14:26:28 -0500

    fix typo in shell alias

--- config/home/bash.nix
+++ config/home/bash.nix
@@ -26,7 +26,7 @@ in {
     shellAliases = {
       sv = "sudo nvim";
       flake-rebuild = "nh os switch --specialisation ${hostname}-$SPECIALIZATION --hostname ${hostname} $FLAKE";
-      flake-update = "nh os switch ---specialisation ${hostname}-$SPECIALIZATION --hostname ${hostname} --update $FLAKE";
+      flake-update = "nh os switch --specialisation ${hostname}-$SPECIALIZATION --hostname ${hostname} --update $FLAKE";
       nix-gc = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       v = "nvim";
       ls = "lsd";
